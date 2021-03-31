3228294dcf13667ee7cc5ae7866b1c706986efa4
Simon Zaidelman <saidelsam@ya.ru>
2021-03-31 20:21:12 +0500

    fix: added help for tracert.py

--- tracert/tracert.py
+++ tracert/tracert.py
@@ -9,18 +9,27 @@ keys: list[str] = 'query|country|isp|as|asname'.split('|')
 
 
 def traceroute(path: str) -> None:
-    os.system(f"traceroute {path} > trace.txt")
+    os.system(f"traceroute {path} > trace.txt")  # посылаем в систему команду, которая все записывает в файл trace.txt
     print('number', *keys, sep=' | ')
     with open("trace.txt", "r") as trace_file:
         (seq(trace_file.readlines()[2:])
-         .map(lambda s: re.search(r"\([\d.]*\)", s))
-         .filter(lambda s: s is not None)
-         .map(lambda s: requests.get(f'http://ip-api.com/json/{s.group()[1:-1]}?fields=4255743').json())
-         .filter(lambda s: s['status'] != 'fail')
-         .zip_with_index()
-         .map(lambda tup: print(tup[1], *[tup[0][i] for i in keys], sep=' | '))
-         .to_list())
+         .map(lambda s: re.search(r"\([\d.]*\)", s))  # находим все IP-шники в каждой строке
+         .filter(lambda s: s is not None)  # избавляемся от пустых строк
+         .map(lambda s: requests.get(
+            f'http://ip-api.com/json/{s.group()[1:-1]}?fields=4255743').json())  # идем в АПИшку за информацией об IP-адресе
+         .filter(lambda s: s['status'] != 'fail')  # Берем только удавшиеся запросы, т.е. по "белым" IP
+         .zip_with_index()  # для вывода номера
+         .map(lambda tup: print(tup[1], *[tup[0][i] for i in keys],
+                                sep=' | '))  # выводим все поля по ключам, указанным в keys
+         .to_list())  # вызываем все, что мы сделали
 
 
 if __name__ == '__main__':
-    traceroute(sys.argv[1])
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+            print(
+                """Usage: 
+    python tracert.py <domain name/ip adress>
+                """)
+        else:
+            traceroute(sys.argv[1])
